f1432c30cc1de3b33050b78c8368cbb70ed2dd09
rlackdgml <dhdlqkqh2003@naver.com>
2020-09-22 17:19:05 +0900

    노력하자.

--- SpMVC_ReadBook/src/main/java/com/biz/book/mapper/BookDao.java
+++ SpMVC_ReadBook/src/main/java/com/biz/book/mapper/BookDao.java
@@ -1,5 +1,12 @@
 package com.biz.book.mapper;
+import com.biz.book.model.BookVO;
 
-public interface BookDao {
+/*
+ * GenericDao 를 extends 하면 
+ * 기본 CRUD메서드를
+ * 별도로 정의하지 않아도 된다.
+ */
+
+public interface BookDao extends GenericDao<BookVO, Integer> {
 
 }
